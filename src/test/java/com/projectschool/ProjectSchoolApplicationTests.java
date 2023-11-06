8db7e0effe81deb5d26e21bc6a644798fe031083
Larissa Hofeman <larissa.hofeman@acad.edu.br>
2023-11-06 16:29:10 -0300

    setting up project

---
+++ src/test/java/com/projectschool/ProjectSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
